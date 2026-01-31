5e8e08d0dec7b50bfe6390f1a9a127bead808d1a
Dhanraj Singh <72537917+Money-Rules@users.noreply.github.com>
2026-01-31 18:17:37 +0530

    GitHub commit working

---
+++ commit.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+REM If no message provided, use default
+if "%~1"=="" (
+    set "msg=AI update"
+) else (
+    set "msg=%~1"
+)
+
+git add .
+git commit -m "%msg%"
+git push
